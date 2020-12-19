46affb8a1fe5961475718780420b1e62f4dd9b73
aizaz aziz <aizaz.azzi@yahoo.com>
2020-12-19 15:35:32 +0500

    context api 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './stylesheet/styles.css';
+import { Home } from './components/Home';
+import { AddEmployees } from './components/AddEmployees';
+import { EditEmployees } from './components/EditEmployees';
+
+
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is a master Branch</h1>
-    </div>
+    <GlobalProvider>
+      <BrowserRouter>
+
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/add" component={AddEmployees} exact />
+        <Route path="/edit/:id" component={EditEmployees} exact />
+      </Switch>
+      </BrowserRouter>
+
+    </GlobalProvider>
   );
 }
 
