478110a07c46da53cce35a476e84e9e5b8edcf5a
aizaz aziz <aizaz.azzi@yahoo.com>
2021-01-26 22:48:32 +0500

    up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import './App.css';
+import React, { Suspense } from "react";
+import { createResource } from "./PersonApi";
+import { Person } from "./Person";
+import { Num } from "./Num";
+import { Name } from "./Name";
+
+const resource = createResource();
 
 function App() {
   return (
     <div className="App">
-      <h1>This is a master Branch</h1>
+      <Suspense fallback={<h1>loading num...</h1>}>
+        <Num resource={resource} />
+      </Suspense>
+      <Suspense fallback={<h1>loading person...</h1>}>
+        <Person resource={resource} />
+      </Suspense>
+      <Suspense fallback={<h1>loading name...</h1>}>
+        <Name resource={resource} />
+      </Suspense>
     </div>
   );
 }
